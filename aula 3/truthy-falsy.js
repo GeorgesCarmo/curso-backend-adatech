ce92c87018c0df0caba964dfa9f2df01737332a1
georges <georgesdc13@gmail.com>
2024-12-26 13:11:57 -0300

    aula 3

---
+++ aula 3/truthy-falsy.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-constant-binary-expression */
+/* eslint-disable no-constant-condition */
+// true
+// truthy: [], {}, número diferente de 0, string não vazia
+// falsy: null, undefined, 0, "", NaN
+
+console.log(!![], !!{}, !!1, !!'afasf', true)
+
+if ([] && {} && 1 && 'afasf' && true) {
+  console.log('tudo é truthy')
+}
+
+console.log(!!null, !!undefined, !!0, !!'', !!NaN)
