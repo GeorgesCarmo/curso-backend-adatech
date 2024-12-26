c9f8f0014936d35d262d12e541f4318f2f06fe6f
georges <georgesdc13@gmail.com>
2024-12-26 13:11:42 -0300

    aula 2

---
+++ aula 2/operadores-booleanos.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-constant-binary-expression */
+// if (true) {
+//   console.log('Sempre aparecerei')
+// }
+
+// if (false) {
+//   console.log('Nunca aparecerei')
+// }
+
+// or/ou: ||
+// and/e : &&
+
+// expressões com ou sempre retornam verdadeiro, a menos que TODOS as expressões sejam falsas
+// console.log(true || true)
+// console.log(true || false)
+// console.log(false || true)
+// console.log(false || false)
+
+// console.log(1 < -20 || 100 < 5 || 30 > 10 || 20 > 50 || 40 > 3000)
+
+console.log(true && true)
+console.log(true && false)
+console.log(false && true)
+console.log(false && false)
